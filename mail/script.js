b40c8695ce856fd3d8ec2ff149aa912c8a8be3b5
federicodececco <federicodececco1@gmail.com>
2024-12-03 14:10:20 +0100

    mail exerices

--- mail/script.js
+++ mail/script.js
@@ -1,4 +1,4 @@
-const teachers = [
+const mailingList = [
     'Nathan@mail.com',
     'Ed@mail.com',
     'Fabio@mail.com',
@@ -7,3 +7,17 @@ const teachers = [
     'Lewis@mail.com',
     'Luca@mail.com'
 ];
+let email = prompt("Inserire mail")
+let flag = 0;
+
+for (let i = 0; i < mailingList.length; i++) {
+    if (mailingList[i].toLowerCase === email.toLowerCase) {
+        flag = 1;
+    }
+}
+if (flag === 0) {
+    console.log("Mail assente dalla lista")
+}
+else {
+    console.log("Mail presente nella lista")
+}
